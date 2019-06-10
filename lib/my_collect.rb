d049fb04f9a4d63925c5f473e0d43babfdb7e7a4
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-10 03:50:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,18 +1,26 @@
 def my_collect(array)
-  if block_given?
-    i = 0
-
-        while i < array.length
-          yield(array[i])
-          i = i + 1
-        end
-
-    array
-  else
-    puts "Hey! No block was given!"
-  end
+  # if block_given?
+  #   i = 0
+  #
+  #       while i < array.length
+  #         yield(array[i])
+  #         i = i + 1
+  #       end
+  #
+  #   array
+  # else
+  #   puts "Hey! No block was given!"
+  # end
 end
 
+worked in the other lesson?
+
+# def my_select(collection)
+#  # your code here!
+#  collection.select do |item|
+#    yield item
+#  end
+# end
 
 
 # def hello_t(array)
