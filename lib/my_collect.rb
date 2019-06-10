57c5ccdc7dcc107cdff71851e000b895d2e9e760
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-10 03:49:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,36 @@
+def my_collect(array)
+  if block_given?
+    i = 0
+
+        while i < array.length
+          yield(array[i])
+          i = i + 1
+        end
+
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
 
 
+
+# def hello_t(array)
+#   if block_given?
+#     i = 0
+#
+#     while i < array.length
+#       yield(array[i])
+#       i = i + 1
+#     end
+#
+#     array
+#   else
+#     puts "Hey! No block was given!"
+#   end
+# end
+
+# collection = [1, 2, 3, 4]
+# my_each(collection) do |i|
+#   puts i
+# end
